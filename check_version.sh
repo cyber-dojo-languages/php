4a388ef6b34b9458d4bf45a26bcc01463279040b
JonJagger <jon@kosli.com>
2026-02-06 16:20:10 +0000

    Upgrade to 8.4.17

--- check_version.sh
+++ check_version.sh
@@ -7,7 +7,7 @@ readonly JSON=`cat docker/image_name.json`
 readonly IMAGE_NAME="${BASH_REMATCH[1]}"
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
-readonly EXPECTED=8.2.30
+readonly EXPECTED=8.4.17
 readonly ACTUAL=$(docker run --rm -i ${IMAGE_NAME} sh -c 'php --version')
 
 if echo "${ACTUAL}" | grep -q "${EXPECTED}"; then
